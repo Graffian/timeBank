239eed371d2747885153d93a06be4f62223334a8
Hiteshpy07 <kumarnayakhitesh@gmail.com>
2025-12-06 20:20:56 +0530

    routing of signup Login

--- app/auth/Login.tsx
+++ app/auth/Login.tsx
@@ -1,13 +1,25 @@
-export default function Login(){
+"use client";
+import React from 'react';
+import type { JSX } from 'react';
+import { useRouter } from 'next/navigation'
+export default function Login(): JSX.Element {
+  const router = useRouter();
+  function handleSingup(){
+    router.push('/auth/SignUp');
+  }
   return(
     <>
-      <div>
-        <input type="email" placeholder='Enter your college Email'/>
-        <input type="password" placeholder='Enter your Password'/>
+    <div className='bg-black h-screen w-full text-white flex justify-center items-center flex-col p-10 gap-5'>
+      <div  className='flex flex-col gap-5 '>
+        <input className='border-slate-600 bg-blue-950 w-[400px] h-[30px] rounded' type="email" placeholder='Enter your college Email'/>
+        <input className='border-slate-600 bg-blue-950 w-[400px] h-[30px] rounded' type="password" placeholder='Enter your Password'/>
+       <span className='text-blue-700 font-mono cursor-pointer hover:underline' onClick={handleSingup}>Don't have a account?Signup</span>
       </div>
+
       <div>
-        <button className="cursor-pointer">Login</button>
+        <button className="cursor-pointer bg-blue-700 rounded text-white w-[200px] h-[40px] hover:bg-white hover:text-black">Login</button>
       </div>
+    </div>
   
     </>
   )

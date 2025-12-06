7f40cda198c4c0462a6f960d263ba08d0bd70ff3
Hiteshpy07 <kumarnayakhitesh@gmail.com>
2025-12-06 20:34:37 +0530

    updates auth

--- app/auth/SignUp/page.tsx
+++ app/auth/SignUp/page.tsx
@@ -1,12 +1,21 @@
-export default function Signup(){
+import { JSX } from "react";
+
+export default function Signup():JSX.Element{
   return(
     <>
-      <p>this is the sign up page </p>
+    <div className="flex flex-col justify-center items-center bg-black text-white h-screen w-full ">
+      <div className="flex flex-col  " >
+        <input className="mt-5 bg-blue-950 w-[400px] h-[45x] p-2 rounded" type="email" placeholder='Enter your college Email'/>
+        <input className="mt-5 bg-blue-950 w-[400px] h-[45x] p-2 rounded" type="password" placeholder='Enter your Password'/>
+        <input className="mt-5 bg-blue-950 w-[400px] h-[45x] p-2 rounded" placeholder='Enter your full name'/>
+        <input className="mt-5 bg-blue-950 w-[400px] h-[45x] p-2 rounded" placeholder='Enter your department'/>
+        <input className="mt-5 bg-blue-950 w-[400px] h-[45x] p-2 rounded" placeholder='Enter your college regd num'/>
+      </div>
+      <div>
+        <button className="cursor-pointer bg-blue-700 text-white hover:bg-white hover:text-black rounded mt-5 w-[100px] p-2">Sign Up</button>
+      </div>
+    </div>
   
     </>
   )
 }
-
-
-
-
